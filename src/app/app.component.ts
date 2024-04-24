b0386802c5abffb938d0df492d9e6bb86eff5453
jimderson28 <jim.chu28@gmail.com>
2024-04-24 20:11:24 +0800

    feat(product): 利用雙向繫結設定產品顯示狀態

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,7 +15,4 @@ export class AppComponent {
   isShow=true;
   imgUrl='https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img';
 
-  onIsShowChange(isShow: boolean): void{
-    this.isShow = isShow;
-  }
 }
