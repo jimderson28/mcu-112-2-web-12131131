f17efb19ac00d93bf5c8aa943f624f0106cf935b
jimderson28 <jim.chu28@gmail.com>
2024-05-01 20:45:15 +0800

    feat(router): 切換產品清單與登入頁面實作

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { ProductCardListComponent } from './product-card-list/product-card-list.component';
 import { Product } from './model/product';
 import { HeaderComponent } from './header/header.component';
+import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
