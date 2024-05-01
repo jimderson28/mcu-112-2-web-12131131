b143fc9688c54322a9da5856583dfe1a0342d34c
jimderson28 <jim.chu28@gmail.com>
2024-05-01 18:30:02 +0800

    feat(product):建立產品資料模型

---
+++ src/app/model/product.ts
@@ -0,0 +1,24 @@
+import { Data } from "@angular/router";
+export class Product {
+  constructor(initData?: Partial<Product>){
+    Object.assign(this, initData);
+    this.createDate=new Date();
+
+  }
+  id!: number;
+
+  name!: string;
+
+  authors!: string;
+
+  company!: string;
+
+  isShow!: boolean;
+
+  imgUrl!: string;
+
+  createDate!: Date;
+
+  price!: number;
+
+}
