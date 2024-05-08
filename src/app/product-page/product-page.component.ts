9bc17f5917f3057ab5de5178878b547388fdcae3
jimderson28 <jim.chu282gmail.com>
2024-05-08 18:42:10 +0800

    feat(product-page): 新增產品功能實作

--- src/app/product-page/product-page.component.ts
+++ src/app/product-page/product-page.component.ts
@@ -22,6 +22,19 @@ products!: Product[];
   this.products = this.ProductService.getList();
 }
 
+onAdd(): void{
+  const product = new Product({
+    name: '書籍 Z',
+    authors: ['作者甲','作者乙','作者丙'],
+    company: '博碩文化',
+    isShow: true,
+    imgUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
+    createDate: new Date(),
+    price: 10000,
+  });
+  this.ProductService.add(product);
+}
+
 onEdit(product: Product): void {
   this.router.navigate(['product', 'form', product.id]);
 }
