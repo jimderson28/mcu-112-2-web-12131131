ab0422a1fc10ad9c66fad72ad74d0df2e0a2f373
jimderson28 <jim.chu28@gmail.com>
2024-05-15 19:36:42 +0800

    feat(product): 產品表單頁面適用預載資料

--- src/app/product-form-page/product-form-page.component.ts
+++ src/app/product-form-page/product-form-page.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { JsonPipe } from '@angular/common';
+import { Component, OnInit, inject } from '@angular/core';
+import { ActivatedRoute, Data } from '@angular/router';
+import { map } from 'rxjs';
+import { Product } from '../model/product';
 
 @Component({
   selector: 'app-product-form-page',
   standalone: true,
-  imports: [],
+  imports: [JsonPipe],
   templateUrl: './product-form-page.component.html',
   styleUrl: './product-form-page.component.css'
 })
-export class ProductFormPageComponent {
+export class ProductFormPageComponent implements OnInit{
+private readonly routes = inject(ActivatedRoute);
 
+product!: Product;
+
+  ngOnInit(): void{
+  this.routes.data.pipe(
+    map(({ product }: Data ) => product)
+    ).subscribe((product) => this.product = product);
+  }
 }
