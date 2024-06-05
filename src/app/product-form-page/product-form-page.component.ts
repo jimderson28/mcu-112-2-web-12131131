c381473a7d72804925759bf856a31d0110f58c18
jimderson28 <jim.chu28@gamil.com>
2024-06-05 19:50:23 +0800

    feat(product-form): 建立產品表單基本欄位定義

--- src/app/product-form-page/product-form-page.component.ts
+++ src/app/product-form-page/product-form-page.component.ts
@@ -3,16 +3,26 @@ import { Component, OnInit, inject } from '@angular/core';
 import { ActivatedRoute, Data } from '@angular/router';
 import { map } from 'rxjs';
 import { Product } from '../model/product';
+import { IProductForm } from '../interface/product-form.interface';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-product-form-page',
   standalone: true,
-  imports: [JsonPipe],
+  imports: [JsonPipe, ReactiveFormsModule],
   templateUrl: './product-form-page.component.html',
   styleUrl: './product-form-page.component.css'
 })
 export class ProductFormPageComponent implements OnInit{
 private readonly routes = inject(ActivatedRoute);
+
+form = new FormGroup<IProductForm>({
+  id: new FormControl<number | null>(null),
+  name: new FormControl<string | null>(null),
+  company: new FormControl<string | null>(null),
+  isShow: new FormControl<boolean>(false, { nonNullable: true }),
+  price: new FormControl<string | null>(null),
+});
 
 product!: Product;
 
